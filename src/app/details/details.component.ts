ba2425b2723a89467c68ffe321e7bab19b6593d2
electronicraghavendra@gmail.com <electronicraghavendra@gmail.com>
2018-05-08 04:17:56 +0530

    second page initial

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,7 +7,7 @@ import { AppRouteConfig } from '../app.router-config';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  Overview:boolean = true;
   constructor(private goto: AppRouteConfig) { }
 
   MockData = [{
@@ -20,5 +20,38 @@ export class DetailsComponent {
     'title': 'Overspeeding Vehicles',
     'count': 2
   }];
+
+  AllVehicles = [
+    {
+      'title': 'Bus #8754',
+      'location': 'Pipeline West, Srinagar,Banashankari',
+      'city': 'Bengaluru,Karnataka 560026',
+      'icon': '../../assets/Moving.png',
+      'status': 'Moving'
+    },
+    {
+      'title': 'Bus #7669',
+      'location': 'Magadi Road,Herohalli,Tunganagara',
+      'city': 'Bengaluru,Karnataka 560026',
+      'icon': '../../assets/Moving.png',
+      'status': 'Moving'
+    },
+    { 'title': 'Bus #5456',
+    'location': 'Chord Rd,Hampi Nagar,RPC Layout,',
+    'city': 'Bengaluru,Karnataka 560104',
+    'icon': '../../assets/Stop.png',
+    'status': 'Stop'
+  },{
+    'title': 'Bus #3432',
+    'location': 'No.193,80 Feet Road,2nd Stage,2nd Block',
+    'city': 'Bengaluru,Karnataka 560104',
+    'icon': '../../assets/Moving.png',
+    'status': 'Moving'
+  }];
+
+
+  show() {
+    this.Overview = !this.Overview;
+  }
 
 }
